0f1058029431339235bf486f5e82ff36c99ae471
Joongi Kim <joongi@lablup.com>
2018-07-31 00:22:30 +0900

    transformer: Adjust runner script to match our environment

--- translation/tensorflow/run.sh
+++ translation/tensorflow/run.sh
@@ -5,9 +5,13 @@ set -e
 SEED=$1
 QUALITY=$2
 
-cd /research/transformer
+#cd /research/transformer
 
-export PYTHONPATH=/research/transformer/transformer:$PYTHONPATH
-python3 process_data.py --raw_dir /raw_data/ --data_dir processed_data
+#export PYTHONPATH=/research/transformer/transformer:$PYTHONPATH
+export PYTHONPATH=./transformer:$PYTHONPATH
+python3 process_data.py --raw_dir ../raw_data/ --data_dir processed_data
 
-python3 transformer/transformer_main.py --random_seed=$SEED --data_dir=processed_data/ --model_dir=model --params=big --bleu_threshold $QUALITY --bleu_source=newstest2014.en --bleu_ref=newstest2014.de
+python3 transformer/transformer_main.py --random_seed=$SEED \
+  --data_dir=processed_data/ --model_dir=model --params=big \
+  --train_steps=1000
+  #--bleu_threshold $QUALITY --bleu_source=newstest2014.en --bleu_ref=newstest2014.de
